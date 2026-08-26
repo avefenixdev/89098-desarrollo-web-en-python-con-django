34ac86b65403281340b77f530c0d25ebb25d7e8f
Ave Fenix Dev <avefenixdev@gmail.com>
2026-08-26 16:20:59 -0300

    Lista las urls (rutas) y los templates del proyecto

--- clase-05/01-orm-formularios-admin/tienda/tienda/urls.py
+++ clase-05/01-orm-formularios-admin/tienda/tienda/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("categorias/", include('categorias.urls', namespace='categorias_app') ),
+    path("clientes/", include('clientes.urls', namespace='clientes_app') ),
+    path("productos/", include('productos.urls', namespace='productos_app') )
 ]
