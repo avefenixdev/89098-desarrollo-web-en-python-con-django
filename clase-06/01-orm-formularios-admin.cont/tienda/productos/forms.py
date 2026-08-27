de9e551a33d4bf985e5c69fc7fb2654d890af40c
Ave Fenix Dev <avefenixdev@gmail.com>
2026-08-27 16:39:33 -0300

    Trabajando con la lista de productos

--- clase-06/01-orm-formularios-admin.cont/tienda/productos/forms.py
+++ clase-06/01-orm-formularios-admin.cont/tienda/productos/forms.py
@@ -8,10 +8,57 @@ class ProductoForm(forms.ModelForm):
         model = Producto
         fields = [
             "nombre",
+            "categoria",
             "descripcion",
-            "precio",
             "color",
             "stock",
-            "categoria"
+            "precio"
         ]
+
+        widgets = {
+
+            "nombre": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Nombre del producto. ej: PC, Notebook"
+                }
+            ),
+
+            "color": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Ingrese color del producto. ej: rojo, blanco"
+                }
+            ),
+
+            "descripcion": forms.Textarea(
+                attrs={
+                    "class": 'form-control',
+                    "rows": 4,
+                    "placeholder": "Escriba un texto descriptivo del producto"
+                }
+            ),
+
+            "precio": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                    "step": '1',
+                    "min": "0"
+                }
+            ),
+
+            "stock": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                    "min": "0"
+                }
+            ),
+            "categoria": forms.Select(
+                attrs={
+                    "class": "form-select"
+                }
+            )
+
+
+        }
 
